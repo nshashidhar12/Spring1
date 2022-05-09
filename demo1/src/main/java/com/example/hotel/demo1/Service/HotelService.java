bbf217044b89bfb8e6c2908f6fed66b4bb3ec5f6
nshashidhar12 <104413671+nshashidhar12@users.noreply.github.com>
2022-05-09 10:42:56 +0530

    Update HotelService.java

--- demo1/src/main/java/com/example/hotel/demo1/Service/HotelService.java
+++ demo1/src/main/java/com/example/hotel/demo1/Service/HotelService.java
@@ -12,8 +12,8 @@ import com.example.hotel.demo1.Entity.Hotel;
 public class HotelService {
 
 	public List<Hotel> hotels = new ArrayList<>(Arrays.asList
-			(new Hotel(1,"Suraj",200),
-			(new Hotel(2,"Ajantha",500))));
+			(new Hotel(1,"Suraj",200)),
+			(new Hotel(2,"Ajantha",500)),(new Hotel(3,"City inn",300)));
 	
 	public List<Hotel> getHotelList(){
 		return hotels;
